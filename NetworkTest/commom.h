a6a25a789572b5ec8d5b66e556b64604c2f4b75c
wangxiwuen <32030117+xiuwen233@users.noreply.github.com>
2020-10-16 16:24:27 +0800

    Add files via upload

---
+++ NetworkTest/commom.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#define U1_RESET                              "01"
+#define U1_OPENLIGHT                  "04"
+#define U1_CLOSELIGHT                "05"
+#define U1_OPENSTEAM                 "10"
+#define U1_CLOSESTEAM               "11"
+#define U1_UPSUCTION                  "12"
+#define U1_DOWNSUCTION          "13"
+#define U1_MATUP                           "14"
+#define U1_MATDOWN                   "15"
+#define U1_PRESSURE                      "16"
+#define U1_CYLINDER                     "17"
+
+
+
+#define U2_RESET                             "01"
+#define U2_UPPRESS                        "10"
+#define U2_UPPRESS                        "11"
+
